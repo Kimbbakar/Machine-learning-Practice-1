95356ba5fd4b99b4ec8b1b333917292f43c5133c
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-16 23:09:28 +0600

    save the test and train data

--- Lecture 2.py
+++ Lecture 2.py
@@ -1,9 +1,24 @@
 from sklearn.datasets import load_iris
 
-# import pandas as pd
-# import numpy as np
+import pandas as pd
+import numpy as np
 
 iris = load_iris()
 
 print iris.feature_names
 print iris.target_names
+
+print iris.data[0] 
+
+test_idx = [0,50,100] 
+
+# Training data 
+
+train_target = np.delete(iris.target,test_idx )
+train_data = np.delete(iris.data,test_idx,axis = 0 )
+
+
+# test data 
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+ 
