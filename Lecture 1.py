d36026d8d1a93595831ad94a531f6de7edb23095
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-16 22:03:19 +0600

    set classifier

--- Lecture 1.py
+++ Lecture 1.py
@@ -1,8 +1,17 @@
 from sklearn import tree
+import numpy as np
 
 # label 0 = apple
 # label 1 = orange 
 
-features = [ [140,1],[130,1],[150,0],[170] ]
-labels = [0,0,1,1]
+features = np.array([ [140,1],[130,1],[150,0],[170,0] ] )
+labels = np.array([0,0,1,1])
 
+
+#decision tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features,labels)
+
+print clf.feature_importances_
+
+print clf.predict( [ [150,0] ] )
