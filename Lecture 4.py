7162056b1a67ef1f46341f740f97da8006859cff
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-17 14:03:01 +0600

    Replace DecisionTreeClassifier with KNeighborsClassifier

--- Lecture 4.py
+++ Lecture 4.py
@@ -10,9 +10,16 @@ from sklearn.cross_validation import train_test_split
 
 x_train ,x_test,y_train,y_test = train_test_split(x,y,test_size = .5 )
 
-from sklearn import tree
+# # using DecisionTreeClassifier
+# from sklearn import tree
+# my_classifier = tree.DecisionTreeClassifier()
 
-my_classifier = tree.DecisionTreeClassifier()
+# using KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
+my_classifier = KNeighborsClassifier()
+
+
+
 my_classifier = my_classifier.fit(x_train,y_train)
 
 
