b118521a199946c7251bc5931b055b410cffd230
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-19 23:07:19 +0600

    Closest distance pradiction class

--- Lecture 5.py
+++ Lecture 5.py
@@ -1,20 +1,42 @@
+from scipy.spatial import distance
 import random
 
 #This lecture is about writing classifier by own 
 
+def euc(a,b):
+	return distance.euclidean(a,b)
 
 class ScrappyKNN():
 	def fit(self,x_train,y_train):
 		self.x_train = x_train
 		self.y_train = y_train
 
-	def predict(self,x_test):
-		predictios = []
-		print "**hi"
+	def random_predict(self,x_test):
+		predictios = [] 
 		for row in x_test:
 			label = random.choice(self.y_train)
 			predictios.append(label)
 		return predictios
+
+	def predict(self,x_test):
+		predictios = [] 
+		for row in x_test:
+			label = self.close(row)
+			predictios.append(label)
+		return predictios
+
+	def close(self, row ):
+
+		dis = 123456789
+		idx = 0
+
+		for i in range(0,len(self.x_train ) ):
+
+			if euc(x_train[i],row )<dis :
+				dis=euc(x_train[i],row )
+				idx = i
+
+		return self.y_train[idx]
 
 
 #import a dataset
